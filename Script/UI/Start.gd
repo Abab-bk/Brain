3ba797ef18f45a0750a070902e00b08c0d0faaf6
Hey R You Are <1470997723@qq.com>
2023-08-31 19:03:19 +0800

    修改game over界面，修改主界面

--- Script/UI/Start.gd
+++ Script/UI/Start.gd
@@ -3,7 +3,8 @@ extends Control
 @onready var _start_game:TextureButton = %StartGame
 @onready var _option:TextureButton = %Option
 @onready var _quit:TextureButton = %Quit
-@onready var _rank_list: TextureButton = %RankList
+@onready var _rank_list:TextureButton = %RankList
+@onready var _background_animation:AnimationPlayer = $TextureRect/AnimationPlayer
 
 var _game_world := preload("res://Scene/World.tscn")
 
@@ -22,3 +23,8 @@ func _ready() -> void:
     _rank_list.pressed.connect(func():
         RankList.visible = true
         )
+    
+    _background_animation.animation_finished.connect(func(_anim_name:String):
+        _background_animation.play_backwards("BackgroundMove")
+        )
+    
