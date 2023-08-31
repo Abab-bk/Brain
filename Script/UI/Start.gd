107c9b8c6531e150a506ab52fa37bdce0d85f0a9
Hey R You Are <1470997723@qq.com>
2023-08-31 19:11:05 +0800

    修改主界面背景实现方式（shader）

--- Script/UI/Start.gd
+++ Script/UI/Start.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var _option:TextureButton = %Option
 @onready var _quit:TextureButton = %Quit
 @onready var _rank_list:TextureButton = %RankList
-@onready var _background_animation:AnimationPlayer = $TextureRect/AnimationPlayer
 
 var _game_world := preload("res://Scene/World.tscn")
 
@@ -23,8 +22,3 @@ func _ready() -> void:
     _rank_list.pressed.connect(func():
         RankList.visible = true
         )
-    
-    _background_animation.animation_finished.connect(func(_anim_name:String):
-        _background_animation.play_backwards("BackgroundMove")
-        )
-    
